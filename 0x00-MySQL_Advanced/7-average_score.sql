6d268bc69324077fbe30712c9d199311fd104b3f
eddybrownent <ekaranja.28@gmail.com>
2023-12-16 09:09:02 +0000

    SQL script that creates a stored procedure ComputeAverageScoreForUser that computes and store the average score for a student

---
+++ 0x00-MySQL_Advanced/7-average_score.sql
@@ -0,0 +1,22 @@
+-- Drop the existing procedure if it exists
+-- Set the delimiter for procedure creation
+-- Update the users table with the calculated average scorre
+-- Create the new procedure
+DROP PROCEDURE IF EXISTS ComputeAverageScoreForUser;
+
+DELIMITER $$
+
+CREATE PROCEDURE ComputeAverageScoreForUser(
+    IN user_id INT
+)
+BEGIN
+    UPDATE users
+    SET average_score = (
+        SELECT AVG(score)
+        FROM corrections
+        WHERE corrections.user_id = user_id
+    )
+    WHERE id = user_id;
+END $$
+DELIMITER ;
+
