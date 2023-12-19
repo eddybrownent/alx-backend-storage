48c8405f037df2fff17c1ab6b3fa86ce1fdd98ec
edwin karanja mburu <edwin@eddy-kali.browns.com>
2023-12-19 12:25:49 +0300

    this function returns the list of schools having a specific topic

---
+++ 0x01-NoSQL/11-schools_by_topic.py
@@ -0,0 +1,20 @@
+#!/usr/env python3
+"""
+Function that returns the list of school
+having a specific topic
+"""
+
+
+def schools_by_topic(mongo_collection, topic):
+    """
+    List of school having a specific tpoic
+
+    Args:
+        mongo_collection: mongo collection object
+        topic: the topict to check of
+
+    Return:
+        list: list of the schools with that topic
+    """
+    ls = mongo_collection.find({"topics": topic})
+    return ls
