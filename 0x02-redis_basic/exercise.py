b96774cff91719e29aa682e6608375579e030337
edwin karanja mburu <edwin@eddy-kali.browns.com>
2023-12-20 17:30:01 +0300

    added a method that Retrieves the data stored in RedisDB which converts data back to the desired format

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -4,7 +4,7 @@ Writing strings to Redis
 """
 from uuid import uuid4
 import redis
-from typing import Union
+from typing import Union, Callable, Optional, Union
 
 
 class Cache:
@@ -36,3 +36,48 @@ class Cache:
         self._redis.set(key, data)
 
         return key
+
+    def get(self, key: str,
+            fn: Optional[Callable] = None) -> Union[str,
+                                                    bytes,
+                                                    int, float, None]:
+        """
+        Retrieve data from Redis using the given key
+
+        Args:
+            key: key of the data in redisDB
+            fn: function to convert retrived data. None is default
+
+        Returns:
+            The retrieved data, converted by the function with
+        """
+        value = self._redis.get(key)
+
+        if value is not None and fn is not None:
+            return fn(value)
+
+        return value
+
+    def get_str(self, key: str) -> Union[str, None]:
+        """
+        gets string from redis using the given key
+
+        Args:
+            key: the key holding the string in redisdb
+
+        Return:
+            The retrieved string or None if key doesnt exist
+        """
+        return self.get(key, str)
+
+    def get_int(self, key: str) -> Union[int, None]:
+        """
+        Gets the int from redis using the given key
+
+        Args:
+            key: key related to the integer in Redis
+
+        Return:
+            the retrieved integer or None if key doesnt exist
+        """
+        return self.get(key, int)
